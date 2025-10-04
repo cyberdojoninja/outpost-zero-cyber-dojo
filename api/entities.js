b8de93a7449e4ab2a02087984110795782764b68
Dojo Sensai <114306105+cyberdojoninja@users.noreply.github.com>
2025-10-04 11:01:25 -0400

    Add files via upload

---
+++ api/entities.js
@@ -0,0 +1,201 @@
+import { base44 } from './base44Client';
+
+
+export const SecurityEvent = base44.entities.SecurityEvent;
+
+export const Incident = base44.entities.Incident;
+
+export const ThreatIntelligence = base44.entities.ThreatIntelligence;
+
+export const UserBehavior = base44.entities.UserBehavior;
+
+export const SOARPlaybook = base44.entities.SOARPlaybook;
+
+export const ComplianceControl = base44.entities.ComplianceControl;
+
+export const Misconfiguration = base44.entities.Misconfiguration;
+
+export const Integration = base44.entities.Integration;
+
+export const TabletopExercise = base44.entities.TabletopExercise;
+
+export const EndpointData = base44.entities.EndpointData;
+
+export const CloudAsset = base44.entities.CloudAsset;
+
+export const UserRole = base44.entities.UserRole;
+
+export const PredictiveAlert = base44.entities.PredictiveAlert;
+
+export const OrganizationSettings = base44.entities.OrganizationSettings;
+
+export const Investigation = base44.entities.Investigation;
+
+export const ThreatFeed = base44.entities.ThreatFeed;
+
+export const AutomatedResponse = base44.entities.AutomatedResponse;
+
+export const FalsePositiveFeedback = base44.entities.FalsePositiveFeedback;
+
+export const CIPipeline = base44.entities.CIPipeline;
+
+export const Subscription = base44.entities.Subscription;
+
+export const Project = base44.entities.Project;
+
+export const VulnerabilityAssessment = base44.entities.VulnerabilityAssessment;
+
+export const CDMCompliance = base44.entities.CDMCompliance;
+
+export const Notification = base44.entities.Notification;
+
+export const VendorAdvisory = base44.entities.VendorAdvisory;
+
+export const DisasterRecoveryStatus = base44.entities.DisasterRecoveryStatus;
+
+export const DataIngestionMetrics = base44.entities.DataIngestionMetrics;
+
+export const CustomQueryLanguage = base44.entities.CustomQueryLanguage;
+
+export const SystemArchitecture = base44.entities.SystemArchitecture;
+
+export const CommunityKnowledge = base44.entities.CommunityKnowledge;
+
+export const TrainingVideo = base44.entities.TrainingVideo;
+
+export const PhysicalSecurityEvent = base44.entities.PhysicalSecurityEvent;
+
+export const GapAnalysisFinding = base44.entities.GapAnalysisFinding;
+
+export const CounterIntelCase = base44.entities.CounterIntelCase;
+
+export const AIModelMetrics = base44.entities.AIModelMetrics;
+
+export const TrainingContent = base44.entities.TrainingContent;
+
+export const LearningInsight = base44.entities.LearningInsight;
+
+export const OrganizationalKnowledge = base44.entities.OrganizationalKnowledge;
+
+export const MSPClient = base44.entities.MSPClient;
+
+export const ExecutiveMetrics = base44.entities.ExecutiveMetrics;
+
+export const SystemPerformance = base44.entities.SystemPerformance;
+
+export const IntegrationMarketplace = base44.entities.IntegrationMarketplace;
+
+export const AuditTrail = base44.entities.AuditTrail;
+
+export const ZeroTrustMetrics = base44.entities.ZeroTrustMetrics;
+
+export const QuantumReadiness = base44.entities.QuantumReadiness;
+
+export const IoTDevice = base44.entities.IoTDevice;
+
+export const FederatedLearning = base44.entities.FederatedLearning;
+
+export const AIAdvisory = base44.entities.AIAdvisory;
+
+export const IAMIntegration = base44.entities.IAMIntegration;
+
+export const MobileApp = base44.entities.MobileApp;
+
+export const SSOIntegration = base44.entities.SSOIntegration;
+
+export const InfrastructureNode = base44.entities.InfrastructureNode;
+
+export const CloudConfiguration = base44.entities.CloudConfiguration;
+
+export const ThreatPredictionMarket = base44.entities.ThreatPredictionMarket;
+
+export const CyberInsuranceRating = base44.entities.CyberInsuranceRating;
+
+export const DeepfakeDetection = base44.entities.DeepfakeDetection;
+
+export const QuantumMigrationPlan = base44.entities.QuantumMigrationPlan;
+
+export const CyberImmuneSystem = base44.entities.CyberImmuneSystem;
+
+export const BlockchainEvidence = base44.entities.BlockchainEvidence;
+
+export const SocialEngineeringProfile = base44.entities.SocialEngineeringProfile;
+
+export const CyberCreditScore = base44.entities.CyberCreditScore;
+
+export const AttackSimulation = base44.entities.AttackSimulation;
+
+export const DeceptionHoneypot = base44.entities.DeceptionHoneypot;
+
+export const HomomorphicAnalysis = base44.entities.HomomorphicAnalysis;
+
+export const DecentralizedIdentity = base44.entities.DecentralizedIdentity;
+
+export const CyberResilienceMetric = base44.entities.CyberResilienceMetric;
+
+export const CyberAwarenessTraining = base44.entities.CyberAwarenessTraining;
+
+export const UserCyberProfile = base44.entities.UserCyberProfile;
+
+export const PlatformMetrics = base44.entities.PlatformMetrics;
+
+export const Licensee = base44.entities.Licensee;
+
+export const APIKey = base44.entities.APIKey;
+
+export const APILog = base44.entities.APILog;
+
+export const FrameworkControl = base44.entities.FrameworkControl;
+
+export const ThreatActorProfile = base44.entities.ThreatActorProfile;
+
+export const AIThreatCorrelation = base44.entities.AIThreatCorrelation;
+
+export const BusinessRiskMetrics = base44.entities.BusinessRiskMetrics;
+
+export const DeploymentTask = base44.entities.DeploymentTask;
+
+export const SecurityToolIntegration = base44.entities.SecurityToolIntegration;
+
+export const PersonnelSecurityClearance = base44.entities.PersonnelSecurityClearance;
+
+export const MobileDevice = base44.entities.MobileDevice;
+
+export const MDMIntegration = base44.entities.MDMIntegration;
+
+export const StripeSubscription = base44.entities.StripeSubscription;
+
+export const MicrosoftPartnership = base44.entities.MicrosoftPartnership;
+
+export const ISPPartner = base44.entities.ISPPartner;
+
+export const GatewayThreat = base44.entities.GatewayThreat;
+
+export const BranchOffice = base44.entities.BranchOffice;
+
+export const NetworkPolicy = base44.entities.NetworkPolicy;
+
+export const PlatformGuide = base44.entities.PlatformGuide;
+
+export const Glossary = base44.entities.Glossary;
+
+export const VideoServiceIntegration = base44.entities.VideoServiceIntegration;
+
+export const GeneratedVideo = base44.entities.GeneratedVideo;
+
+export const CustomSDK = base44.entities.CustomSDK;
+
+export const SDKInstallation = base44.entities.SDKInstallation;
+
+export const SDKDevelopmentRequest = base44.entities.SDKDevelopmentRequest;
+
+export const AgentDeployment = base44.entities.AgentDeployment;
+
+export const AgentHeartbeat = base44.entities.AgentHeartbeat;
+
+export const AgentInstallerPackage = base44.entities.AgentInstallerPackage;
+
+
+
+// auth sdk:
+export const User = base44.auth;
---
+++ api/functions.js
@@ -0,0 +1,23 @@
+import { base44 } from './base44Client';
+
+
+export const verifyStripeSession = base44.functions.verifyStripeSession;
+
+export const createCheckoutSession = base44.functions.createCheckoutSession;
+
+export const generateVideo = base44.functions.generateVideo;
+
+export const azureSentinelConnector = base44.functions.azureSentinelConnector;
+
+export const cerebraSecConnector = base44.functions.cerebraSecConnector;
+
+export const renderVideoWithService = base44.functions.renderVideoWithService;
+
+export const syncSDKDeveloperPortal = base44.functions.syncSDKDeveloperPortal;
+
+export const submitSDKRequest = base44.functions.submitSDKRequest;
+
+export const agentHeartbeat = base44.functions.agentHeartbeat;
+
+export const agentRegistration = base44.functions.agentRegistration;
+
